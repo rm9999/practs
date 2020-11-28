46dfbb7e28145b46ac807159a177d5b58bfd00ca
Raghu <Raghu@Raghu-PC>
2020-11-28 22:35:03 +0530

    1st done

---
+++ com.practice/src/test/java/com/practice/CmTest.java
@@ -0,0 +1,9 @@
+package com.practice;
+
+public class CmTest {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+
+}
--- com.practice/src/test/java/com/practice/PractTest.java
+++ com.practice/src/test/java/com/practice/PractTest.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Pract {
+public class PractTest {
 
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.co.in/");
-		System.out.println("google open...........!!!!!!!!!!");
+		System.out.println("google opened...........!!!!!!!!!!");
 	}
 
 }
