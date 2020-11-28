3976aa1c6c11f02acaec73743dfa78e09b64a427
Raghu <Raghu@Raghu-PC>
2020-11-28 21:51:14 +0530

    done success

---
+++ com.practice/src/test/java/com/practice/Pract.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Pract {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		System.out.println("google open............!!!!!!!!!!!");
+	}
+
+}
